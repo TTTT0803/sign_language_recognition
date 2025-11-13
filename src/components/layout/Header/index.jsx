f7b6daa5e7970af0a6d0cf1f64e931480cb2db30
TTTT0803 <trinhthanhthao2k5@gmail.com>
2025-11-13 22:20:58 +0700

    add img logo in header

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,13 +1,17 @@
-// src/components/layout/Header/index.jsx
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import checklogo from '../../../assets/images/checklogo.png';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
         <NavLink to="/" className="logo">
-          <span role="img" aria-label="logo" style={{ fontSize: '2rem' }}>☝️</span>
+            <img 
+              src={checklogo} 
+              alt="logo" 
+              style={{ width: '4rem', height: '4rem' }} 
+            />
         </NavLink>
         <nav className="nav-menu">
           <NavLink to="/">Trang chủ</NavLink>
